9dc2fda36f00f3cf53c314aabb13c455be40a6d5
Abhishek Koirala <readeravskh@gmail.com>
2025-01-23 10:53:21 +0545

    Calculator 1 complete

--- simplecalculator/app/src/main/java/com/developeravsk/simplecalculator/MainActivity.java
+++ simplecalculator/app/src/main/java/com/developeravsk/simplecalculator/MainActivity.java
@@ -1,20 +1,52 @@
 package com.developeravsk.simplecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// Activity class
 public class MainActivity extends AppCompatActivity {
 
+    //ui defined component
+    Button add, subtract;
+    TextView result;
+    int counter = 0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //introduce your layout
         setContentView(R.layout.activity_main);
-        
+
+        //object initialization using id
+        add = findViewById(R.id.add_data);
+        subtract = findViewById(R.id.subtract_data);
+        result = findViewById(R.id.display_value);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // define what to do when add button is clicked
+                counter++;
+                result.setText("" + counter);
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //define what to do when subtract button is clicked
+                counter--;
+                result.setText("" + counter);
+            }
+        });
     }
 }
